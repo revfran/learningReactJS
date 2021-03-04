30bab9667d6d6f4e055a1aa04e36d50a2291953f
Lisbona, Jose <Jose.Lisbona@adidas.com>
2021-03-04 07:42:14 +0100

    Add properties

--- learning-reactjs/src/App.js
+++ learning-reactjs/src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 
-function Header() {
-  return (<header><h1>"Learn React, by Revfran"</h1></header>)
+function Header(props) {
+  return (<header><h1>"Learn React, by {props.owner}"</h1></header>)
 }
 
 function Main() {
   return (<section><p>This is just the body of the page</p></section>)
 }
 
-function Footer() {
-  return (<footer><p>It's cool to have a footer in place</p></footer>)
+function Footer(props) {
+  return (<footer><p>It's cool to have a footer in place.</p><p>Copyright {props.year}</p></footer>)
 }
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header owner="revfran"/>
       <Main />
-      <Footer />
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
