7862d4e5ebddaa2e0423187c8879566a7736ecf7
Lisbona, Jose <Jose.Lisbona@adidas.com>
2021-03-04 07:21:52 +0100

    Injecting a custom element

--- learning-reactjs/src/index.js
+++ learning-reactjs/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("h1",
+  { style: { color: "blue" }},
+  "Hello World! I'm building a ReactJS app!"
+  ),
   document.getElementById('root')
 );
 
